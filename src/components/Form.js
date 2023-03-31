26395f61d5ab77717d30860d1c236087e59d0c27
Amber Hunt <amberhunt955@gmail.com>
2023-03-31 12:17:33 -0400

    test commit, fixed form error

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,8 @@ import { Button } from "@mui/material";
 function Form() {
     return (
         <form>
-            <label for="name">Your Name: <input id="name" name="name" type="text" required /></label>
-            <label for="email">Your Email: <input id="email" name="email" type="email" required /></label>
+            <label htmlFor="name">Your Name: <input id="name" name="name" type="text" required /></label>
+            <label htmlFor="email">Your Email: <input id="email" name="email" type="email" required /></label>
             <textarea id="message" name="message" rows="15" cols="50" required></textarea>
             <Button variant="contained" type="submit" value="Submit">Submit</Button>
         </form>
