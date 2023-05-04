72a95ed3ed5133f263d3c1806f730280bbcc0532
Amber Hunt <amberhunt955@gmail.com>
2023-05-04 18:39:55 -0400

    added per scholas final project to portfolio

--- src/components/main-sections/Projects.js
+++ src/components/main-sections/Projects.js
@@ -3,6 +3,7 @@ import ProjectCard from "../ProjectCard";
 
 // import images
 import spaceBattle from "../../assets/images/space-battle.jpg"
+import eventTracker from "../../assets/images/event-tracker.png"
 
 function Projects() {
   return (
@@ -12,6 +13,14 @@ function Projects() {
       <p id="project-text">Explore some of my recent projects and discover how I'm evolving my skills as a web developer.</p>
 
       <div id="project-display">
+      <ProjectCard
+          name="Event Tracker"
+          src={eventTracker}
+          description="Full stack app tracking the birthdays, anniversaries and special occasions of people you care about most. Technologies: MongoDB, Express, React, Node."
+          link="https://event-tracker.herokuapp.com/"
+          code="https://github.com/amberhunt955/special-day-tracker"
+        />
+
         <ProjectCard
           name="Maze Duel: Race to the Opposite Corner"
           src="https://user-images.githubusercontent.com/122638466/223304807-bfc8bff4-7b25-4883-a8e1-98b161638638.png"
@@ -19,6 +28,7 @@ function Projects() {
           link="https://amberhunt955.github.io/maze-game/"
           code="https://github.com/amberhunt955/maze-game"
         />
+
         <ProjectCard
           name="Space Battle"
           src={spaceBattle}
